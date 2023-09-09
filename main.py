002b3e3bb21c16ee4e62cd683607b44a548dbd94
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-09-09 18:26:13 -0500

    Verificacion sizeMatrix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 global mainWindow
 
@@ -9,11 +10,31 @@ mainWindow = tk.Tk()
 mainWindow.title('Proyecto - Matematica Computacional - ( 2023 - 2 ) - Grupo 5')
 mainWindow.geometry("800x600")
 
+sizeMatrix = tk.StringVar()
+sizeMatrix.set("0")
+
+def checkEntrySize():    
+    print(sizeMatrix.get())
+    sizeMatrixEntryData = sizeMatrix.get()
+    sizeMatrixEntryData.replace(" ", "")
+
+    if int(sizeMatrixEntry) == 0:
+        print(True)
+
+    if not str(sizeMatrixEntryData).isdigit():
+        print(False)
+
+    sizeMatrixEntryData = int(sizeMatrixEntryData)
+    if not (sizeMatrixEntryData >= 5 and sizeMatrixEntryData <= 10):
+        print(False)
+
+    print(True)
+
 #Tamaño matriz
 tk.Label(mainWindow, text = "Size of Matrix: ").grid(row = 0)
-sizeMatrixEntry = tk.Entry(mainWindow).grid(row = 0, column = 1)
+sizeMatrixEntry = tk.Entry(mainWindow, textvariable = sizeMatrix).grid(row=0, column=1)
 
-
+button = tk.Button(mainWindow, text="Submit", bg='bisque3',width=15, command=checkEntrySize).grid(row = 0, column = 2)
 
 #Ejecucion applicacion
 mainWindow.mainloop()
