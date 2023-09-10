b5bb39db9fa2c5470b4833daf1ce28d28aabfcdf
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-09-10 14:51:13 -0500

    File Changes

--- main.py
+++ main.py
@@ -1,52 +1,53 @@
 import tkinter as tk 
 
-#Creacion del frame
-global mainWindow
+#Archivos adicionales
+import dataInput as dt
 
-#Inicio applicacion
+#Creacion del frame 
+global mainWindow
 mainWindow = tk.Tk()
 
-#Configuracion
+#Variables Interfaz
+#MatrixData
+global matrixData
+matrixData = []
+
+#EntriesMatrix
+global entriesMatrix
+entriesMatrix = []
+
+#Configuracion del app
 mainWindow.title('Proyecto - Matematica Computacional - ( 2023 - 2 ) - Grupo 5 - Seccion SS41')
 mainWindow.geometry("800x600")
 mainWindow.resizable(False, False)
 
-#Inicializacion de la variable sizeMatrix
+
+#Variables
+#sizeMatrix
 sizeMatrix = tk.StringVar()
 sizeMatrix.set("0")
-
-#Informacion acerca del labelMatrix
+#labelMatrix
 infMatrix = tk.StringVar()
 infMatrix.set("Ingrese la informacion de la matriz")
 
-#Informacion matriz
-global matrixData
-global entriesMatrix
+def verificacionSizeMatrix():
+    # Output Data
+    # 0 El entry tiene 0
+    # 1 El entry tiene un dato no valido 
+    # 2 El entry tiene un dato valido
 
-matrixData = []
-entriesMatrix = []
-
-def checkEntrySize():
-    #Return
-    # 0 Inicializacion
-    # 1 Invalid Int
-    # 2 True
-        
-    print(sizeMatrix.get())
     sizeMatrixData = sizeMatrix.get()
     sizeMatrixData = sizeMatrixData.replace(" ", "")
-
-    print(type(sizeMatrixData))
 
     if not str(sizeMatrixData).isdigit():
         infMatrix.set("El valor de la matriz ingresada no es valida. Ingrese el valor entre 5 a 15")
         return 1
-
-    if int(sizeMatrixData) == 0:
+    
+    sizeMatrixData = int(sizeMatrixData)
+    if sizeMatrixData == 0:
         infMatrix.set("El valor de la matriz ingresada no es valida. Ingrese el valor entre 5 a 15")
         return 0
 
-    sizeMatrixData = int(sizeMatrixData)
     if not (sizeMatrixData >= 5 and sizeMatrixData <= 15):
         infMatrix.set("El valor de la matriz ingresada no es valida. Ingrese el valor entre 5 a 15")
         return 1
@@ -54,59 +55,30 @@ def checkEntrySize():
     infMatrix.set("El valor de la matriz exitoso")
     return 2
 
-
-
-def checkMatrix():
-    if checkEntrySize() == 2:
-        #if len(matrixData) != 0:
-        #    deleteMatrix()
-        genMatrix(int(sizeMatrix.get()))
-
-def genMatrix():
-    posX = 100
-    posY = 100
+#GenMatrix
+def matrixDataControlller():
+    if len(matrixData) == 0:
+        dt.generateMatrix(mainWindow,
+                      matrixData, entriesMatrix)
     
-    for indexRow in range(15):
-        matrixData.append([])
-        entriesMatrix.append([])
-        for indexColumn in range(15):
-            matrixData[indexRow].append(tk.StringVar())
-            entriesMatrix[indexRow].append(tk.Entry(mainWindow, textvariable= matrixData[indexRow][indexColumn], width = 2))
-            entriesMatrix[indexRow][indexColumn].place(x = posX + indexColumn * 20, y = posY + indexRow * 20)
+    if verificacionSizeMatrix() == 2:
+        dt.hideMatrix(mainWindow,
+                      matrixData, entriesMatrix)
+        dt.showMatrix(int(sizeMatrix.get()), mainWindow,
+                      matrixData, entriesMatrix)
 
-def showMatrix(sizeMatrix):
-    posX = 100
-    posY = 100
-    
-    for indexRow in range(sizeMatrix):
-        for indexColumn in range(sizeMatrix):
-            entriesMatrix[indexRow][indexColumn].place(x = posX + indexColumn * 20, y = posY + indexRow * 20)
-    
-#Max size 15x15
-def hideMatrix():
-    for indexRow in range(15):
-        for indexColumn in range(15):
-            entriesMatrix[indexRow][indexColumn].place_forget()        
+#Ingreso datos tamaño matriz
+tk.Label(mainWindow, text = "Tamaño de la matriz: ").place(x = 0, y = 0)
+sizeMatrixEntry = tk.Entry(mainWindow, textvariable = sizeMatrix).place(x = 120, y = 0)
+
+#Informacion del dato ingresado
+tk.Label(mainWindow, text = "Ingrese la informacion de la matriz", textvariable = infMatrix).place(x = 0, y = 45)
                     
-#Tamaño matriz
-tk.Label(mainWindow, text = "Size of Matrix: ").place(x = 0, y = 0)
-sizeMatrixEntry = tk.Entry(mainWindow, textvariable = sizeMatrix).place(x = 90, y = 0)
-button = tk.Button(mainWindow, text="Generate Matrix",
-                   width=15, command=checkMatrix).place(x=220, y=0)
-
-button1 = tk.Button(mainWindow, text="Generate Matrix",
-                    width=15, command=genMatrix).place(x=220, y=40)
-
-button1 = tk.Button(mainWindow, text="Generate Matrix",
-                   width=15, command=hideMatrix).place(x=220, y=60)
+#Generacion de la matriz
+button = tk.Button(mainWindow, text="Generate Matrix", width=15,
+                   command=matrixDataControlller).place(x=260, y=40)
 
 
-tk.Label(mainWindow, text = "Ingrese la informacion de la matriz", textvariable = infMatrix).place(x = 0, y = 25)
-
-
-
-#Configurar los tamaños de la pantalla
-#mainWindow.rowconfigure(0, weight = 1)
 
 #Ejecucion applicacion
 mainWindow.mainloop()
